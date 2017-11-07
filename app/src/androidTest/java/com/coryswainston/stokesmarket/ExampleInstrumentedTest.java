116f0ce6b9f13d2557236be394cb3baf616d3390
Cory Swainston <swainston.cory89@gmail.com>
2017-11-06 18:58:46 -0700

    Rename to Stokes Market
    Also changed package names

--- app/src/androidTest/java/com/coryswainston/stokesmarket/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coryswainston/stokesmarket/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.coryswainston.stokesmarket", appContext.getPackageName());
+        assertEquals("com.coryswainston.myapplication", appContext.getPackageName());
     }
 }
